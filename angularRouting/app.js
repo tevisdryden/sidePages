66670b7f4ca56b862100130eb1026aee14b949ba
Tevis Dryden <TevisDryden@gmail.com>
2017-10-13 14:41:17 -0600

    add more

--- angularRouting/app.js
+++ angularRouting/app.js
@@ -3,5 +3,26 @@ angular.module('News', [])
     '$scope',
     function($scope){
       $scope.test = 'Hello world!';
+      $scope.posts = [
+	    'Post 1',
+	    'Post 2',
+	    'Post 3',
+	    'Post 4',
+	    'Post 5'
+	  ];
+	  $scope.posts = [
+	    {title:'Post 1', upvotes:5},
+	    {title:'Post 2', upvotes:6},
+	    {title:'Post 3', upvotes:1},
+	    {title:'Post 4', upvotes:4},
+	    {title:'Post 5', upvotes:3}
+	  ];
+	  $scope.addPost = function() {
+	    $scope.posts.push({title:'A new post',upvotes:0});
+	  };
+	  $scope.addPost = function() {
+	    $scope.posts.push({title:$scope.formContent,upvotes:0});
+	    $scope.formContent='';
+	  };
     }
  	]);
