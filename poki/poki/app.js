171daddc00c91c5c3ab207b7ccef7a51c76fffed
Tevis Dryden <TevisDryden@gmail.com>
2017-10-30 22:57:46 -0600

    fix?

--- poki/poki/app.js
+++ poki/poki/app.js
@@ -1,60 +1,60 @@
-var app = window.angular.module('app', [])
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-app.factory('pokemonFetcher', pokemonFetcher)
-app.controller('mainCtrl', mainCtrl)
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-function pokemonFetcher ($http) {
+var app = express();
 
-  var API_ROOT = 'pokemon'
-  return {
-    get: function () {
-      return $http
-        .get(API_ROOT)
-        .then(function (resp) {
-          return resp.data
-        })
-    },
-    tryit: function() {
-      var politics = "https://zlzlap7j50.execute-api.us-east-1.amazonaws.com/prod";
-      return $http
-        .get(politics)
-        .then(function (resp) {
-          console.log("Get Worked");
-          console.log(resp.data);
-          return resp.data
-        })
-    }
-  }
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-}
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-function mainCtrl ($scope, pokemonFetcher, $http) {
+app.use('/', routes);
+app.use('/users', users);
 
-  $scope.pokemon = []
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-  pokemonFetcher.get()
-    .then(function (data) {
-      $scope.pokemon = data
-    })
+// error handlers
 
-  pokemonFetcher.tryit()
-    .then(function (data) {
-      console.log("tryit");
-      console.log(data);
-    })
-
-  $scope.addPoki = function() {
-    var formData = {name:$scope.Name,avatarUrl:$scope.Url};
-    console.log(formData);
-    var pokiURL = 'pokemon';
-    $http({
-       url: pokiURL,
-       method: "POST",
-       data: formData
-    }).success(function(data, status, headers, config) {
-      console.log("Post worked");
-    }).error(function(data, status, headers, config) {
-      console.log("Post failed");
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
-}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
---
+++ poki/poki/public/app.js
@@ -0,0 +1,60 @@
+var app = window.angular.module('app', [])
+
+app.factory('pokemonFetcher', pokemonFetcher)
+app.controller('mainCtrl', mainCtrl)
+
+function pokemonFetcher ($http) {
+
+  var API_ROOT = 'pokemon'
+  return {
+    get: function () {
+      return $http
+        .get(API_ROOT)
+        .then(function (resp) {
+          return resp.data
+        })
+    },
+    tryit: function() {
+      var politics = "https://zlzlap7j50.execute-api.us-east-1.amazonaws.com/prod";
+      return $http
+        .get(politics)
+        .then(function (resp) {
+          console.log("Get Worked");
+          console.log(resp.data);
+          return resp.data
+        })
+    }
+  }
+
+}
+
+function mainCtrl ($scope, pokemonFetcher, $http) {
+
+  $scope.pokemon = []
+
+  pokemonFetcher.get()
+    .then(function (data) {
+      $scope.pokemon = data
+    })
+
+  pokemonFetcher.tryit()
+    .then(function (data) {
+      console.log("tryit");
+      console.log(data);
+    })
+
+  $scope.addPoki = function() {
+    var formData = {name:$scope.Name,avatarUrl:$scope.Url};
+    console.log(formData);
+    var pokiURL = 'pokemon';
+    $http({
+       url: pokiURL,
+       method: "POST",
+       data: formData
+    }).success(function(data, status, headers, config) {
+      console.log("Post worked");
+    }).error(function(data, status, headers, config) {
+      console.log("Post failed");
+    });
+}
+}
