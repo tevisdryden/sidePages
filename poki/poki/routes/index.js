a8644fba0c6df71f9bfc8e17c0557cfbe352ea1c
Tevis Dryden <TevisDryden@gmail.com>
2017-10-30 23:23:46 -0600

    full

--- poki/poki/routes/index.js
+++ poki/poki/routes/index.js
@@ -45,11 +45,11 @@ router.get('/politics', function(req,res) {
   request(politics).pipe(res);
 });
 
-// router.post('/pokemon', function(req, res) {
-//     console.log("In Pokemon Post");
-//     console.log(req.body);
-//     pokemon.push(req.body);
-//     res.end('{"success" : "Updated Successfully", "status" : 200}');
-// }); 
+router.post('/pokemon', function(req, res) {
+    console.log("In Pokemon Post");
+    console.log(req.body);
+    pokemon.push(req.body);
+    res.end('{"success" : "Updated Successfully", "status" : 200}');
+}); 
 
 module.exports = router;
