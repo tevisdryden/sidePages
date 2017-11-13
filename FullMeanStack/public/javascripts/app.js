8b3de9db14f232b1bb5e06be1f8892c39b71e263
Tevis Dryden <TevisDryden@gmail.com>
2017-11-13 14:21:49 -0700

    stuff

---
+++ FullMeanStack/public/javascripts/app.js
@@ -0,0 +1,31 @@
+angular.module('comment', [])
+.controller('MainCtrl', [
+  '$scope','$http',
+  function($scope,$http){
+    $scope.comments = [];
+    $scope.addComment = function() {
+      var newcomment = {title:$scope.formContent,upvotes:0};
+      $scope.formContent='';
+      $http.post('/comments', newcomment).success(function(data){
+        $scope.comments.push(data);
+      });
+    };
+    $scope.upvote = function(comment) {
+      return $http.put('/comments/' + comment._id + '/upvote')
+        .success(function(data){
+          console.log("upvote worked");
+          comment.upvotes = data.upvotes;
+        });
+    };
+	$scope.incrementUpvotes = function(comment) {
+	  $scope.upvote(comment);
+    };
+    $scope.getAll = function() {
+      return $http.get('/comments').success(function(data){
+        angular.copy(data, $scope.comments);
+      });
+    };
+    $scope.getAll();
+
+  }
+]);
